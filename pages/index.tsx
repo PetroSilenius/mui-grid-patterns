6a333c6fd178d158cc439c35aae6866b27622c92
Petro Silenius <petro.silenius@gmail.com>
2022-08-24 20:42:28 +0300

    Simplify index page, remove about page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
-import * as React from 'react';
 import type { NextPage } from 'next';
 import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
-import Link from '../src/Link';
-import ProTip from '../src/ProTip';
-import Copyright from '../src/Copyright';
 
 const Home: NextPage = () => {
   return (
@@ -17,16 +13,10 @@ const Home: NextPage = () => {
           flexDirection: 'column',
           justifyContent: 'center',
           alignItems: 'center',
-        }}
-      >
+        }}>
         <Typography variant="h4" component="h1" gutterBottom>
           MUI v5 + Next.js with TypeScript example
         </Typography>
-        <Link href="/about" color="secondary">
-          Go to the about page
-        </Link>
-        <ProTip />
-        <Copyright />
       </Box>
     </Container>
   );
